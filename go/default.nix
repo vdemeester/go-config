2821c8a65a923f06857811128d30596c84be55fc
Vincent Demeester <vincent@sbr.pm>
2017-03-01 22:42:39 +0100

    Update default.nix 👼
    This is not yet perfect but meh..
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- go/default.nix
+++ go/default.nix
@@ -1,10 +1,15 @@
-{ }:
-
 let
-  pkgs = import <nixpkgs> { };
+  _pkgs = import <nixpkgs> {};
 in
-  pkgs.stdenv.mkDerivation {
-    name = "go projects";
+{ pkgs ? import (_pkgs.fetchFromGitHub { owner = "NixOS";
+                                         repo = "nixpkgs-channels";
+                                         rev = "a9584c9510771f96594b4461e9ea546a75bf59d4";
+                                         sha256 = "0in14479h1bjpvh7ncsc57vla06x57cnhnlypb3rml78nkgn0fyk";
+                                       }) {}
+}:
+
+  pkgs.stdenv.mkDerivation rec {
+    name = "go-projects";
     env = pkgs.buildEnv { name = name; paths = buildInputs; };
     buildInputs = [
       pkgs.go_1_7
@@ -14,7 +19,6 @@ in
       pkgs.gcc
       pkgs.gotools
       pkgs.golint
-      pkgs.goimports
       pkgs.godef
     ];
   }
