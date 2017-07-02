bbfeb46fff2788689ba0403f453beec5e937e3d0
Vincent Demeester <vincent@sbr.pm>
2017-07-02 14:25:56 +0200

    Update go projects nix file
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- go/default.nix
+++ go/default.nix
@@ -1,25 +1,25 @@
 let
-  _pkgs = import <nixpkgs> {};
+	_pkgs = import <nixpkgs> {};
 in
-{ pkgs ? import (_pkgs.fetchFromGitHub { owner = "NixOS";
-                                         repo = "nixpkgs-channels";
-					 rev = "0d4431cfe90b2242723ccb1ccc90714f2f68a609";
-					 sha256 = "0iil6dx91widz66avnbs4m8lhygmadhyma1m2kbq57iwj73yql3w";
-                                       }) {}
+	{ pkgs ? import (_pkgs.fetchFromGitHub {
+		owner = "NixOS";
+		repo = "nixpkgs-channels";
+		rev = "1add3519dffca4fa40e539904c3c9522582167f7";
+		sha256 = "1qmwpvp5s6639qf3llc3yy5qnmdkka7pw1lzr4h73cn2439jg0cn";
+	}) {}
 }:
 
-  pkgs.stdenv.mkDerivation rec {
-    name = "go-projects";
-    env = pkgs.buildEnv { name = name; paths = buildInputs; };
-    buildInputs = [
-      pkgs.go_1_8
-      pkgs.vndr
-      pkgs.gnumake
-      pkgs.glide
-      pkgs.gcc
-      pkgs.gotools
-      pkgs.golint
-      pkgs.godef
-      pkgs.go-bindata
-    ];
-  }
+pkgs.stdenv.mkDerivation rec {
+	name = "go-projects";
+	env = pkgs.buildEnv { name = name; paths = buildInputs; };
+	buildInputs = [
+	pkgs.go_1_8
+	pkgs.vndr
+	pkgs.gnumake
+	pkgs.glide
+	pkgs.gcc
+	pkgs.gotools
+	pkgs.golint
+	pkgs.godef
+];
+}
